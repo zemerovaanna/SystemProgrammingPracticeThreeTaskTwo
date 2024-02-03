f7cbcf04c5d948d42838230f737bf559575b83e1
cvv_code <cvv_code@gmail.com>
2024-02-03 11:11:45 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppBFXYZ.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+#define E 2.71828182845904523536
+#define X 1.54
+#define Y 3.26
+#define Z 15 / X + pow(E,Y)
+#define B (X + pow(Y,3)) / (X + pow(Z,2))
+  
+int main() {
+	setlocale(LC_ALL, "rus");
+	cout << "Вычислить b = f(x, y, z), где z = wf(x, y)" << endl;
+	cout << "b = " << B << endl;
+	return 0;
+}
